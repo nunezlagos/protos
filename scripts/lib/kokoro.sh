6c8c912ebbceabca9ccf49a83d3a395383f79643
nunezlagos <asistente.nunez@gmail.com>
2026-07-18 12:02:33 -0400

    feat: change default voice to am_fenrir for balanced robotic/human feel

--- scripts/lib/kokoro.sh
+++ scripts/lib/kokoro.sh
@@ -94,7 +94,7 @@ KOKORO_MODEL="${KOKORO_DIR}/kokoro-${KOKORO_TAG}.onnx"
 KOKORO_VOICES="${KOKORO_DIR}/voices-${KOKORO_TAG}.bin"
 
 # Voice: af_sarah, af_nicole, af_bella, af_heart, am_michael, am_fenrir, etc.
-KOKORO_VOICE_DEFAULT="af_sarah"
+KOKORO_VOICE_DEFAULT="am_fenrir"
 
 # Language: en-us, en-gb, es, fr, pt, it, ja, zh, hi, ko
 KOKORO_LANGUAGE="es"
